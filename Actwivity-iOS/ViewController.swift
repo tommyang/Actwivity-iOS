f89df73e2b20eb8ec4b5f6b63400a97bc1d2aa42
Tommy Yang <yt940928@gmail.com>
2018-07-23 16:18:24 -0700

    Initial Commit

---
+++ Actwivity-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actwivity-iOS
+//
+//  Created by Tommy Yang on 7/23/18.
+//  Copyright © 2018 tOmMyanG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
